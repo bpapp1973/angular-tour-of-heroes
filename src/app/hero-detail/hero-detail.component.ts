d77f2fa8d4d603a5a8163b9661a959eb49e4f17a
bpapp <bela.papp@outlook.hu>
2017-11-10 12:04:35 +0100

    initial

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
